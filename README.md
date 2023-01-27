9328bac354fde029e9151f62e5fffed727ae0d95
Flavio Martins <flaviomartins@acm.org>
2023-01-27 19:04:10 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# 2022-aid-mac-m1
+
+To run this:
+
+`docker-compose up --build`
+
+
+To start from scratch:
+
+`docker-compose up --build --force-recreate --remove-orphans`
---
+++ docker-entrypoint-initdb.d/init-user-db.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+	CREATE USER bd WITH PASSWORD 'bd';
+	CREATE DATABASE bd;
+	GRANT ALL PRIVILEGES ON DATABASE bd TO bd;
+EOSQL

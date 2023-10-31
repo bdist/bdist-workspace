d9d5addf99230cdcf2d91c01ff7e5dee2ec5034d
Flavio Martins <flavio.f.martins@tecnico.ulisboa.pt>
2023-10-31 15:49:55 +0000

    bundle an app with a single healthcheck endpoint

--- app/app.py
+++ app/app.py
@@ -2,19 +2,9 @@
 import os
 from logging.config import dictConfig
 
-import psycopg
-from flask import flash
 from flask import Flask
 from flask import jsonify
-from flask import redirect
-from flask import render_template
-from flask import request
-from flask import url_for
-from psycopg.rows import namedtuple_row
 
-
-# postgres://{user}:{password}@{hostname}:{port}/{database-name}
-DATABASE_URL = os.environ.get("DATABASE_URL", "postgres://db:db@postgres/db")
 
 dictConfig(
     {
@@ -40,105 +30,10 @@ app.config.from_prefixed_env()
 log = app.logger
 
 
-def is_decimal(s):
-    """Returns True if string is a parseable float number."""
-    try:
-        float(s)
-        return True
-    except ValueError:
-        return False
-
-
-@app.route("/", methods=("GET",))
-@app.route("/accounts", methods=("GET",))
-def account_index():
-    """Show all the accounts, most recent first."""
-
-    with psycopg.connect(conninfo=DATABASE_URL) as conn:
-        with conn.cursor(row_factory=namedtuple_row) as cur:
-            accounts = cur.execute(
-                """
-                SELECT account_number, branch_name, balance
-                FROM account
-                ORDER BY account_number DESC;
-                """,
-                {},
-            ).fetchall()
-            log.debug(f"Found {cur.rowcount} rows.")
-
-    return render_template("account/index.html", accounts=accounts)
-
-
-@app.route("/accounts/<account_number>/update", methods=("GET",))
-def account_update_view(account_number):
-    """Show the page to update the account balance."""
-
-    with psycopg.connect(conninfo=DATABASE_URL) as conn:
-        with conn.cursor(row_factory=namedtuple_row) as cur:
-            account = cur.execute(
-                """
-                SELECT account_number, branch_name, balance
-                FROM account
-                WHERE account_number = %(account_number)s;
-                """,
-                {"account_number": account_number},
-            ).fetchone()
-            log.debug(f"Found {cur.rowcount} rows.")
-
-    return render_template("account/update.html", account=account)
-
-
-@app.route("/accounts/<account_number>/update", methods=("POST",))
-def account_update_save(account_number):
-    """Update the account balance."""
-
-    balance = request.form["balance"]
-
-    error = None
-
-    if not balance:
-        error = "Balance is required."
-    if not is_decimal(balance):
-        error = "Balance is required to be decimal."
-
-    if error is not None:
-        flash(error)
-    else:
-        with psycopg.connect(conninfo=DATABASE_URL) as conn:
-            with conn.cursor(row_factory=namedtuple_row) as cur:
-                cur.execute(
-                    """
-                    UPDATE account
-                    SET balance = %(balance)s
-                    WHERE account_number = %(account_number)s;
-                    """,
-                    {"account_number": account_number, "balance": balance},
-                )
-            conn.commit()
-        return redirect(url_for("account_index"))
-
-
-@app.route("/accounts/<account_number>/delete", methods=("POST",))
-def account_delete(account_number):
-    """Delete the account."""
-
-    with psycopg.connect(conninfo=DATABASE_URL) as conn:
-        with conn.cursor(row_factory=namedtuple_row) as cur:
-            cur.execute(
-                """
-                DELETE FROM account
-                WHERE account_number = %(account_number)s;
-                """,
-                {"account_number": account_number},
-            )
-        conn.commit()
-    return redirect(url_for("account_index"))
-
-
 @app.route("/ping", methods=("GET",))
 def ping():
     log.debug("ping!")
-    return jsonify({"message": "pong!", "status": "success"})
+    return jsonify({"message": "pong!"}), 200
 
 
 if __name__ == "__main__":
